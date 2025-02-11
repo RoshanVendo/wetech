d56d1575e8ac1e815cb42f8fc15b1fb5993db821
RoshanVendo <mohamed.roshan@vendomnia.com>
2025-02-11 12:31:49 +0400

    Create git-webhook.php

---
+++ git-webhook.php
@@ -0,0 +1,8 @@
+<?php
+// Log webhook requests
+file_put_contents("webhook.log", file_get_contents("php://input") . PHP_EOL, FILE_APPEND);
+
+// Run Git pull
+exec("cd /web/test/wetech && git pull origin main 2>&1", $output);
+echo implode("\n", $output);
+?>
