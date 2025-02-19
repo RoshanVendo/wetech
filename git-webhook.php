39845ddfb317df673756b32b584f1016ee8a1474
RoshanVendo <mohamed.roshan@vendomnia.com>
2025-02-19 12:38:14 +0400

    Update git-webhook.php

--- git-webhook.php
+++ git-webhook.php
@@ -2,7 +2,7 @@
 // Log webhook requests
 file_put_contents("webhook.log", file_get_contents("php://input") . PHP_EOL, FILE_APPEND);
 
-// Run Git pull
+// Run Git pulll
 exec("git pull origin main 2>&1", $output);
 echo implode("\n", $output);
 ?>
