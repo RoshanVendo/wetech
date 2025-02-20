e63d5abe22ca0034b49044b7ce9c53d0544e7f42
RoshanVendo <mohamed.roshan@vendomnia.com>
2025-02-20 10:23:40 +0400

    Update git-webhook.php

--- git-webhook.php
+++ git-webhook.php
@@ -2,7 +2,11 @@
 // Log webhook requests
 file_put_contents("webhook.log", file_get_contents("php://input") . PHP_EOL, FILE_APPEND);
 
+// Force Git to use the correct SSH key
+putenv("GIT_SSH_COMMAND=ssh -i /var/www/sunniva-solar.de/home/c543261_w3_ssh1/.ssh/id_rsa -o IdentitiesOnly=yes");
+
 // Run Git pull
-exec("git pull origin main 2>&1", $output);
+exec("/usr/bin/git -C /web/test/wetech pull origin main 2>&1", $output);
+
 echo implode("\n", $output);
 ?>
