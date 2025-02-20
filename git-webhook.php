3722fcb07963552611efa58c1ead0938e0393cd3
RoshanVendo <mohamed.roshan@vendomnia.com>
2025-02-20 11:06:58 +0400

    Update git-webhook.php

--- git-webhook.php
+++ git-webhook.php
@@ -2,11 +2,14 @@
 // Log webhook requests
 file_put_contents("webhook.log", file_get_contents("php://input") . PHP_EOL, FILE_APPEND);
 
+// Define the correct Git directory
+$projectDir = "/var/www/clients/client1/web3/web/test/wetech";
+
 // Force Git to use the correct SSH key
 putenv("GIT_SSH_COMMAND=ssh -i /var/www/sunniva-solar.de/home/c543261_w3_ssh1/.ssh/id_rsa -o IdentitiesOnly=yes");
 
-// Run Git pull
-exec("/usr/bin/git -C /web/test/wetech pull origin main 2>&1", $output);
+// Run Git pull as the correct user
+exec("sudo -u web3 /usr/bin/git -C " . escapeshellarg($projectDir) . " pull origin main 2>&1", $output);
 
 echo implode("\n", $output);
 ?>
